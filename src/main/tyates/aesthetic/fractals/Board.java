9e284e51d0fa5f79f83f9cb9cbcf8310a9b632c2
Tyler Yates <tyates@indeed.com>
2021-10-01 13:31:59 -0500

    Proper mouse position

--- src/main/tyates/aesthetic/fractals/Board.java
+++ src/main/tyates/aesthetic/fractals/Board.java
@@ -2,13 +2,55 @@ package tyates.aesthetic.fractals;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Board extends JPanel {
+public class Board extends JPanel implements MouseListener {
+    private int offsetX = 0;
+    private int offsetY = 0;
+
+    public void setMouseOffset(final int offsetX, final int offsetY) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+    }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         g.setColor(Color.BLUE);
         g.fillRect(0, 0, 1000, 100);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println(e.getButton());
+        final Point point = getMousePosition(e);
+        System.out.println(point);
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    private Point getMousePosition(final MouseEvent e) {
+        final Point reportedPoint = e.getPoint();
+        return new Point((int)reportedPoint.getX() - offsetX, (int)reportedPoint.getY() - offsetY);
     }
 }
--- src/main/tyates/aesthetic/fractals/Main.java
+++ src/main/tyates/aesthetic/fractals/Main.java
@@ -1,6 +1,7 @@
 package tyates.aesthetic.fractals;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static final int FRAME_WIDTH = 1280;
@@ -9,9 +10,15 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Aesthetic Fractals");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(FRAME_WIDTH,FRAME_HEIGHT);
 
-        frame.getContentPane().add(new Board());
+        final Board board = new Board();
+        board.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+        frame.getContentPane().add(board);
+        frame.addMouseListener(board);
+        frame.pack();
+
+        final Insets insets = frame.getInsets();
+        board.setMouseOffset(insets.left, insets.top);
 
         frame.setVisible(true);
     }
